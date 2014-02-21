8156ca263240561484f45ed5605eba7658be601b
Alejandro Silva <pm5k.sk@gmail.com>
2014-02-21 01:04:53 -0300

    Dia 4
    ejercicios 13 al 17

---
+++ Excercise 13 Parameters, unpacking, variables/ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+
+puts "the script is called #{$0}"
+puts "your first variable is #{first}"
+puts "your second variables is #{second}"
+puts "your third variable is #{third}"
+
+gets.chomp
